8bc3962ae7c6a1e62bf21d84196568005bea6608
RismaIkaAliwardani <aliwardanirisma@gmail.com>
2019-12-14 14:51:04 +0700

    first commit

---
+++ app/src/main/java/com/example/nitatamringapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.nitatamringapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button masuk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        masuk = findViewById(R.id.masuk);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(a);
+                finish();
+            }
+        });
+
+    }
+}
